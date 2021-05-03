3164dfb7d73783cd3f11ec2ef2e16df62487ef44
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2021-05-03 14:40:49 -0400

    added notations

--- PizzaParlour/Form1.cs
+++ PizzaParlour/Form1.cs
@@ -15,6 +15,7 @@ namespace PizzaParlour
 
     public partial class pizzaParlour : Form
     {
+        //declare global varibles 
         double pizzaPrice = 7.00;
         double numOfToppings;
         double toppingPrice = 1.25;
@@ -29,12 +30,15 @@ namespace PizzaParlour
 
         private void inputButton_Click(object sender, EventArgs e)
         {
+            //check if only numbers have been inputed 
             try
             {
+                //display total price
                 numOfToppings = Convert.ToDouble(inputBox.Text);
                 totalPrice = pizzaPrice + toppingPrice * numOfToppings;
                 outputLabel.Text = $"The price of a pizza with {numOfToppings} toppings is:\n{totalPrice.ToString("C")}";
             }
+            //display message if something other than numbers have been entered 
             catch { outputLabel.Text = "please enter a number"; }
 
         }
