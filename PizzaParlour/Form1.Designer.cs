2076f0fb03ab78c34db99e795a8fdf88b18a89b8
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2021-05-03 14:38:07 -0400

    Add project files.

---
+++ PizzaParlour/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace PizzaParlour
+{
+    partial class pizzaParlour
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlelabel = new System.Windows.Forms.Label();
+            this.toppingsLabel = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titlelabel
+            // 
+            this.titlelabel.BackColor = System.Drawing.Color.DarkRed;
+            this.titlelabel.Font = new System.Drawing.Font("ItalicT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlelabel.Location = new System.Drawing.Point(12, 9);
+            this.titlelabel.Name = "titlelabel";
+            this.titlelabel.Size = new System.Drawing.Size(412, 43);
+            this.titlelabel.TabIndex = 0;
+            this.titlelabel.Text = "Pizza Parlour";
+            this.titlelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // toppingsLabel
+            // 
+            this.toppingsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppingsLabel.ForeColor = System.Drawing.Color.White;
+            this.toppingsLabel.Location = new System.Drawing.Point(13, 85);
+            this.toppingsLabel.Name = "toppingsLabel";
+            this.toppingsLabel.Size = new System.Drawing.Size(229, 43);
+            this.toppingsLabel.TabIndex = 1;
+            this.toppingsLabel.Text = "Number of Toppings";
+            this.toppingsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(248, 93);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 26);
+            this.inputBox.TabIndex = 2;
+            // 
+            // inputButton
+            // 
+            this.inputButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.inputButton.FlatAppearance.BorderSize = 2;
+            this.inputButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputButton.ForeColor = System.Drawing.Color.White;
+            this.inputButton.Location = new System.Drawing.Point(155, 168);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(122, 52);
+            this.inputButton.TabIndex = 3;
+            this.inputButton.Text = "Calculate ";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 269);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(412, 60);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pizzaParlour
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.toppingsLabel);
+            this.Controls.Add(this.titlelabel);
+            this.Name = "pizzaParlour";
+            this.Text = "Pizza Parlour";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titlelabel;
+        private System.Windows.Forms.Label toppingsLabel;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ PizzaParlour/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaParlour
+{
+    
+
+
+    public partial class pizzaParlour : Form
+    {
+        double pizzaPrice = 7.00;
+        double numOfToppings;
+        double toppingPrice = 1.25;
+        double totalPrice;
+
+        public pizzaParlour()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numOfToppings = Convert.ToDouble(inputBox.Text);
+                totalPrice = pizzaPrice + toppingPrice * numOfToppings;
+                outputLabel.Text = $"The price of a pizza with {numOfToppings} toppings is:\n{totalPrice.ToString("C")}";
+            }
+            catch { outputLabel.Text = "please enter a number"; }
+
+        }
+    }
+}
